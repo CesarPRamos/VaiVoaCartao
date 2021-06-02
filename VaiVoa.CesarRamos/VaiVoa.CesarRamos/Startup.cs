5ceb6b2d0c8b96504209c79c27acb282b0ba66b3
Cesarpramos <engcesarramos@gmail.com>
2021-06-02 16:31:04 -0300

    adicionado connectionString

--- VaiVoa.CesarRamos/VaiVoa.CesarRamos/Startup.cs
+++ VaiVoa.CesarRamos/VaiVoa.CesarRamos/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vaivoa.CesarRamos;
 
 namespace VaiVoa.CesarRamos
 {
@@ -25,6 +27,10 @@ namespace VaiVoa.CesarRamos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add banco
+            var connection = Configuration["SqlConnectionString:DefaultConnection"];
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(connection));
+            services.AddControllers();
             services.AddControllers();
         }
 
